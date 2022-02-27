332883fb7f60f667978f611341e94d878c1bad84
dov liberman <doliberm@g.jct.ac.il>
2022-02-27 10:48:28 +0200

    Ray class

---
+++ src/primitives/Ray.java
@@ -0,0 +1,36 @@
+package primitives;
+
+import java.util.Objects;
+
+public class Ray {
+    private Point p0;
+    private Vector dir;
+
+    /**
+     * ctor
+     *
+     * @param p0  sterte point
+     * @param dir direction
+     */
+    public Ray(Point p0, Vector dir) {
+        this.dir = dir.normalize();
+        this.p0 = new Point(p0._xyz);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (!(obj instanceof Ray)) return false;
+        Ray other = (Ray) obj;
+        return this.dir.equals(other.dir) && this.p0.equals(other.p0);
+    }
+
+    @Override
+    public String toString() {
+        return "Ray{" +
+                "p0=" + p0 +
+                ", dir=" + dir +
+                '}';
+    }
+}
