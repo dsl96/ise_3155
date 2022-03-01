90f4f4a91b4167a175feb91e3d42b777d8117ec9
yoel sluzt <sluzt@g.jct.ac.il>
2022-03-01 22:12:17 +0200

    part of PointTest Test

--- unitest/primitives/PointTest.java
+++ unitest/primitives/PointTest.java
@@ -10,6 +10,18 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class PointTest {
 
+    //Point p1 = new Point(1, 2, 3);
+
+
+
+
+
+
+		//if (!(p1.add(new Vector(-1, -2, -3)).equals(new Point(0, 0, 0))))
+        //    out.println("ERROR: Point + Vector does not work correctly");
+
+
+
     @Test
     void distanceSquared() {
     }
@@ -20,6 +32,7 @@ class PointTest {
 
     @Test
     void subtract() {
+        assertEquals(new Vector (1,1,1) ,new Point(2, 3, 4).subtract(new Point(1, 2, 3)),"Point - Point does not work correctly");
     }
 
     /**
