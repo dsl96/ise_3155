892c4edf57035ccd267cdb8d219521f14c4e9a23
yoel sluzt <sluzt@g.jct.ac.il>
2022-04-01 13:22:14 +0300

    ex5

--- src/scene/Scene.java
+++ src/scene/Scene.java
@@ -1,17 +1,46 @@
 package scene;
 
-import elements.AmbientLight;
+import geometries.Intersectable;
+import lighting.AmbientLight;
 import geometries.Geometries;
 import primitives.*;
 
 public class Scene {
+
+
+
     public String name;
-    public Color color;
     public Color background;
-
     public AmbientLight ambientLight;
-    public Geometries geometries;
+    public Geometries geometries ;
 
+    // ***************** Constructor ********************** //
 
+    /**
+     * constructor that get the name of scene
+     * this constructor builds a new empty geometries
+     * @param name
+     */
+    public Scene (String name){
+        this.name=name;
+        geometries = new Geometries();
+    }
+
+    // ***************** Setters ********************** //
+
+    public Scene setAmbientLight(AmbientLight ambientLight) {
+        this.ambientLight = ambientLight;
+        return this;
+    }
+
+    public Scene setBackground(Color background) {
+        this.background = background;
+        return this;
+    }
+
+    public Scene setGeometries(Intersectable... geometries) {
+        this.geometries.add(geometries);
+        return this;
+    }
 
 }
--- unitest/renderer/CameraTest.java
+++ unitest/renderer/CameraTest.java
@@ -18,7 +18,7 @@ class CameraTest {
 
 	/**
 	 * Test method for
-	 * {@link elements.Camera#constructRay(int, int, int, int)}.
+	 * {@link lighting.Camera#constructRay(int, int, int, int)}.
 	 */
 	@Test
 	void testConstructRay() {
