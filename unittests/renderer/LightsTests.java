436a383576f89fd0e119a7188cdf6d3a9918ea50
dov liberman <doliberm@g.jct.ac.il>
2022-04-26 20:07:42 +0300

    light tests

--- unittests/renderer/LightsTests.java
+++ unittests/renderer/LightsTests.java
@@ -163,4 +163,135 @@ public class LightsTests {
 				.writeToImage(); //
 	}
 
+	/**
+	 *  test for multi lights scene
+	 */
+	@Test
+	public void multiLight() {
+
+		 Camera camera3 = new Camera(new Point(0, 0, 0), new Vector(1, 0, 0), new Vector(0, 0, 1)) //
+				.setVPSize(50d, 50) //
+				.setVPDistance(100);
+
+		 Geometry sphere1 = new Sphere(new Point(20,-1.5,0.8),2).
+				 setMaterial(new Material().setKd(  0.85).setKs(0.15 ).setShininess(100)).
+				 setEmission(new Color(yellow).reduce(4.0) );
+
+		 Geometry sphere2 = new Sphere(new Point(30,1.5,0.5),2.6).
+		         setMaterial(new Material().setKd(0).setKs(1).setShininess(300)).
+				 setEmission(new Color( red).scale(2));
+
+
+		 Scene scene3 = new Scene("towSphere")
+				 .setAmbientLight(new AmbientLight(new Color(white).scale(0.8), new Double3(0.8)))
+				 .setBackground(new Color(BLACK).scale(0.7) );
+		scene3.geometries.add(sphere1,sphere2);
+
+
+		 scene3.lights.add(new PointLight(new Color(green).scale(5),new Point(25,0,0))
+				 .setKq(0.7)
+				 .setKl(0.2)
+				 .setKc(0.1));
+
+		scene3.lights.add(new DirectionalLight( new Color( white).reduce(6.2), new Vector(0,-1,1)) );
+
+		scene3.lights.add(new SpotLight(new Color(white).reduce(2), new Point(18.5,-9,8.5)
+				                        ,new Vector(-1.5,7.5,-7.7))  );
+
+
+		ImageWriter imageWriter = new ImageWriter("multilights", 1000, 1000);
+		camera3.setImageWriter(imageWriter) //
+				.setRayTracer(new RayTracerBasic(scene3)) //
+				.renderImage() //
+				.writeToImage(); //
+	}
+
+	/**
+	 * Produce a picture of a two triangles lighted by a narrow spot light
+	 */
+	@Test
+	public void trianglesMultiLight() {
+		scene2.geometries.add(triangle1, triangle2);
+
+
+		scene2.lights.add(new SpotLight(trCL, new Point(10,30,-100), trDL)/*.setNarrowBeam(10)*/.setKl(0.001).setKq(0.00004));
+
+		scene2.lights.add(new DirectionalLight(new Color(white).reduce(2), new Vector(0, 0, -1)));
+		ImageWriter imageWriter = new ImageWriter("trianglesMultiLight", 500, 500);
+		camera2.setImageWriter(imageWriter) //
+				.setRayTracer(new RayTracerBasic(scene2)) //
+				.renderImage() //
+				.writeToImage(); //
+	}
+
+	/**
+	 * Produce a picture of a two triangles lighted by a narrow spot light
+	 */
+	@Test
+	public void trianglesSpotSharp1() {
+		Camera camera3 = new Camera(new Point(3, 3, 100), new Vector(0, 0, -1), new Vector(0, 1, 0)) //
+				.setVPSize(50d, 50) //
+				.setVPDistance(100);
+
+		Geometry triangle1 = new Triangle(new Point(0,0,20),new Point(10,10,0) ,new Point(-10,10,0)).
+				setMaterial(new Material().setKd(  0.1).setKs(0.9 ).setShininess(100)).
+				setEmission(new Color(yellow).reduce(4.0) );
+		Geometry triangle2 = new Triangle(new Point(0,0,20),new Point(-10,10,0) ,new Point(-10,-10,0)).
+				setMaterial(new Material().setKd(  0.1).setKs(0.9 ).setShininess(100)).
+				setEmission(new Color(orange).reduce(4.0) );
+		Geometry triangle3 = new Triangle(new Point(0,0,20),new Point(-10,-10,0) ,new Point(10,-10,0)).
+				setMaterial(new Material().setKd(  0.5).setKs(0.5 ).setShininess(100)).
+				setEmission(new Color(red).reduce(4.0) );
+		Geometry triangle4 = new Triangle(new Point(0,0,20),new Point(10,-10,0) ,new Point(10,10,0)).
+				setMaterial(new Material().setKd(  0.5).setKs(0.5 ).setShininess(100)).
+				setEmission(new Color(blue).reduce(8.0) );
+
+
+
+		Scene scene3 = new Scene("pyramid")
+				.setAmbientLight(new AmbientLight(new Color(white).scale(0.8), new Double3(0.8)))
+				.setBackground(new Color(BLACK).scale(0.7) );
+		scene3.geometries.add(triangle1,triangle2,triangle3,triangle4);
+
+
+
+		scene3.lights.add(new PointLight(new Color(white).reduce(2),new Point(2,2,20))
+				.setKq(0.7)
+				.setKl(0.2)
+				.setKc(0.1));
+
+		//scene3.lights.add(new DirectionalLight( new Color( white).reduce(6.2), new Vector(30,25,-1)) );
+
+		scene3.lights.add(new SpotLight(new Color(white).reduce(2), new Point(0,0,20)
+				,new Vector(0,0,-1))  );
+
+
+
+		ImageWriter imageWriter = new ImageWriter("pyramid", 500, 500);
+		camera3.setImageWriter(imageWriter) //
+				.setRayTracer(new RayTracerBasic(scene3)) //
+				.renderImage() //
+				.writeToImage(); //
+	}
+
+	/*
+	create sphere with multi light
+	 */
+	@Test
+	public void sphereMultiLight() {
+		scene1.geometries.add(sphere);
+		scene1.lights.add(new DirectionalLight(spCL, new Vector(1, 1, -0.5)));
+
+		scene1.lights.add(new SpotLight(spCL, spPL, new Vector(1, 1, -0.5)).setKl(0.001).setKq(0.0001));
+
+		scene1.lights.add(new SpotLight( new Color(green).scale(3), new Point(50, 50, 25), new Vector(-1, -1, -0.5)).setKl(0.001).setKq(0.0001));
+
+
+		ImageWriter imageWriter = new ImageWriter("sphereMultiLight", 500, 500);
+		camera1.setImageWriter(imageWriter) //
+				.setRayTracer(new RayTracerBasic(scene1)) //
+				.renderImage() //
+				.writeToImage(); //
+	}
+
 }
